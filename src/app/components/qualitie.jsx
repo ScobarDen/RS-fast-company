ba57ba538af3fdc8706f4c4b768c6903aca063bd
ScobarDen <gdv201099@gmail.com>
2022-11-22 10:03:23 +0300

    4. Пагинация - 7. [КП] все до задания

--- src/app/components/qualitie.jsx
+++ src/app/components/qualitie.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
 import React from "react";
 const Qualitie = ({ color, name }) => {
     return <span className={"badge m-1 bg-" + color}>{name}</span>;
