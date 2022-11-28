a16962914f3e37d1b72cdb6f225ca61d33a04c39
ScobarDen <gdv201099@gmail.com>
2022-11-28 16:55:26 +0300

    дз, пока только асинхронно юзеры

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Users from "./components/users";
 import api from "./api";
 
 function App() {
-    const [users, setUsers] = useState(api.users.fetchAll());
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        api.users.fetchAll().then((data) => setUsers(data));
+    }, []);
+
     const handleDelete = (userId) => {
         setUsers(users.filter((user) => user._id !== userId));
     };
@@ -19,13 +24,23 @@ function App() {
         console.log(id);
     };
     return (
-        <div>
-            <Users
-                onDelete={handleDelete}
-                onToggleBookMark={handleToggleBookMark}
-                users={users}
-            />
-        </div>
+        <>
+            {users
+                ? (
+                    <Users
+                        onDelete={handleDelete}
+                        onToggleBookMark={handleToggleBookMark}
+                        users={users}
+                    />
+                )
+                : (
+                    <div className="d-flex justify-content-center">
+                        <div className="spinner-border mt-5" role="status">
+                            <span className="sr-only"></span>
+                        </div>
+                    </div>
+                )}
+        </>
     );
 }
 
