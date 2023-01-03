1c3e9eea776ab520f5191073e8afe4d099006454
ScobarDen <gdv-ps@yandex.ru>
2023-01-03 14:06:16 +0300

    delaem2

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -21,6 +21,7 @@ const Login = () => {
             <div className="container mt-5">
                 <div className="row">
                     <div className="col-md-6 offset-md-3 shadow p-4">
+                        <h3 className="mb-4">Edit</h3>
                         <EditUserPage/>
                     </div>
                 </div>
