0558c2b85545483cab9ff3e1d88a640badfa8147
ScobarDen <gdv-ps@mail.ru>
2023-01-16 12:14:14 +0300

    useMemo

--- src/app/components/examples/useMemo/complexCalculate.jsx
+++ src/app/components/examples/useMemo/complexCalculate.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
+function factorial(n) {
+    console.log("factorial");
+    return n ? n * factorial(n - 1) : 1;
+}
+
 const ComplexCalculateExample = () => {
+    const [value, setValue] = useState(100);
+    const [otherState, setOtherState] = useState(false);
+    const fact = useMemo(() => factorial(value), [value]);
+    const buttonColor = otherState ? "primary" : "dark";
+    useEffect(() => console.log("render"));
     return (
         <>
             <CardWrapper>
                 <SmallTitle>Кэширование сложных вычислений</SmallTitle>
+                <p>
+                    {value}! = {fact}
+                </p>
+                <button
+                    className="btn btn-primary ms-md-2"
+                    onClick={() => setValue((prevState) => prevState + 10)}
+                >
+                    Increment
+                </button>
+                <button
+                    className="btn btn-primary ms-md-2"
+                    onClick={() => setValue((prevState) => prevState - 10)}
+                >
+                    Decrement
+                </button>
             </CardWrapper>
             <CardWrapper>
                 <SmallTitle>Зависимость от сторонних setState</SmallTitle>
+                <button
+                    className={`btn btn-${buttonColor} ms-md-2`}
+                    onClick={() => setOtherState((prevState) => !prevState)}
+                >
+                    Поменять цвет кнопки
+                </button>
             </CardWrapper>
         </>
     );
