2a565d3a9fb6fda53bb0d851cd4bcf1fcc9a17e0
ScobarDen <gdv-ps@yandex.ru>
2023-03-02 21:55:52 +0300

    comments

--- src/app/hooks/useComments.jsx
+++ src/app/hooks/useComments.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
+import { useAuth } from "./useAuth";
+import { nanoid } from "nanoid";
 
 const CommentsContext = React.createContext();
 
@@ -10,6 +13,8 @@ export const useComments = () => {
 
 export const CommentsProvider = ({ children }) => {
     // const [isLoading, setLoading] = useState(true);
+    const { userId } = useParams();
+    const { currentUser } = useAuth();
     const [comments, setComments] = useState([]);
     const [error, setError] = useState(null);
     useEffect(() => {
@@ -23,13 +28,24 @@ export const CommentsProvider = ({ children }) => {
         setComments([]);
     }, []);
 
+    async function createComment(data) {
+        const comment = {
+            ...data,
+            _id: nanoid(),
+            pageId: userId,
+            created_at: Date.now(),
+            userId: currentUser._id
+        };
+        console.log(comment);
+    }
+
     // function errorCatcher(error) {
     //     const { message } = error.response.data;
     //     setError(message);
     // }
 
     return (
-        <CommentsContext.Provider value={{ comments }}>
+        <CommentsContext.Provider value={{ comments, createComment }}>
             {children}
         </CommentsContext.Provider>
     );
