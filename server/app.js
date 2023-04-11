bfff0e4120713240cb6b733ae36c270c4a1d988d
ScobarDen <gdv-ps@mail.ru>
2023-04-11 16:41:03 +0300

    env

--- server/app.js
+++ server/app.js
@@ -7,11 +7,25 @@ const PORT = config.get("port") ?? 8080;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 process.env.NODE_ENV === "production" &&
   console.log(chalk.blue("Production environment"));
 process.env.NODE_ENV === "development" &&
   console.log(chalk.blue("Development environment"));
 
-app.listen(PORT, () =>
-  console.log(chalk.green(`Server has been started on port ${PORT}...`))
-);
+async function start() {
+  try {
+    console.log(config.get("mongoUri"));
+    await mongoose.connect(config.get("mongoUri"));
+    app.listen(PORT, () =>
+      console.log(chalk.green(`Server has been started on port ${PORT}...`))
+    );
+  } catch (err) {
+    console.log(chalk.red(`Error: ${err.message}`));
+    process.exit(1);
+  }
+}
+
+start();
