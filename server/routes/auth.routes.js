29b402ab8d152b2eff817671a4ac9f83e5adeca9
ScobarDen <gdv-ps@mail.ru>
2023-04-12 16:37:22 +0300

    auth

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,42 @@
 const express = require("express");
+const User = require("../models/User");
+const chalk = require("chalk");
+const bcrypt = require("bcryptjs");
+const jsonwebtoken = require("jsonwebtoken");
+const expressValidator = require("express-validator");
+const { generateUserData } = require("../utils/helpers");
+const tokenService = require("../services/token.service");
+
 const router = express.Router({ mergeParams: true });
 
 // /api/auth/signUp
-router.post("/signUp", async (req, res) => {});
+router.post("/signUp", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const exitingUser = await User.findOne({ email });
+    if (exitingUser) {
+      return res.status(400).json({
+        error: {
+          message: "EMAIL_EXIST",
+          code: 400,
+        },
+      });
+    }
+    const hashPassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      ...generateUserData(),
+      ...req.body,
+      password: hashPassword,
+    });
+    const tokens = tokenService.generate({ _id: newUser._id });
+    res.status(201).send({ ...tokens, userId: newUser._id });
+  } catch (err) {
+    console.log(chalk.red(`Error: ${err.message}`));
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже...",
+    });
+  }
+});
 router.post("/signInWithPassword", async (req, res) => {});
 router.post("/token", async (req, res) => {});
 
---
+++ server/services/token.service.js
@@ -0,0 +1,11 @@
+class TokenService {
+  //   interface Tokens {
+  //   userId: string
+  //   accessToken: string
+  //   refreshToken: string
+  //   exporesIn: number
+  // }
+  generate() {}
+}
+
+module.exports = new TokenService();
