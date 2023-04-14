2f3567448b4515906c3ba7b33927b79d98d33110
ScobarDen <gdv-ps@mail.ru>
2023-04-14 11:41:00 +0300

    finish back

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,20 +1,48 @@
 const express = require("express");
 const chalk = require("chalk");
-
+const Comment = require("../models/Comment");
+const auth = require("../middleware/auth.midleware");
 const router = express.Router({ mergeParams: true });
 
-router.get("/", async (req, res) => {
-  try {
-  } catch (err) {
-    console.log(chalk.red(`Error: ${err.message}`));
-    res.status(500).json({
-      message: "На сервере произошла ошибка. Попробуйте позже...",
-    });
-  }
-});
+router
+  .route("/")
+  .get(auth, async (req, res) => {
+    try {
+      const { orderBy, equalTo } = req.query;
+      const list = await Comment.find({ [orderBy]: equalTo });
+      res.send(list);
+    } catch (err) {
+      console.log(chalk.red(`Error: ${err.message}`));
+      res.status(500).json({
+        message: "На сервере произошла ошибка. Попробуйте позже...",
+      });
+    }
+  })
+  .post(auth, async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        ...req.body,
+        userId: req.user._id,
+      });
+      res.status(201).send(newComment);
+    } catch (err) {
+      console.log(chalk.red(`Error: ${err.message}`));
+      res.status(500).json({
+        message: "На сервере произошла ошибка. Попробуйте позже...",
+      });
+    }
+  });
 
-router.patch("/:userId", async (req, res) => {
+router.delete("/:commentId", auth, async (req, res) => {
   try {
+    const { commentId } = req.params;
+    const removedComment = await Comment.findById(commentId);
+    if (removedComment.userId.toString() === req.user._id) {
+      await removedComment.remove();
+      return res.send(null);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
   } catch (err) {
     console.log(chalk.red(`Error: ${err.message}`));
     res.status(500).json({
